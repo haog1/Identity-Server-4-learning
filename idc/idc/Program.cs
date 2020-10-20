ae8995c190a38396062e0455066b96244334f415
Tony <9074571+haog1@users.noreply.github.com>
2020-10-20 21:10:07 +1100

    Initial commit

---
+++ idc/idc/Program.cs
@@ -0,0 +1,54 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace idc
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5001");
+            if (disco.IsError)
+            {
+                Console.WriteLine($"Localhost error {disco.Error}");
+                return;
+            }
+
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "511536EF-F270-4058-80CA-1C89C192F69A",
+                Scope = "scope1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine($"Token error {tokenResponse.Error}");
+                return;
+            }
+
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("http://localhost:6001/api/identities");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
